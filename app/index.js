7243588617382b07635133ebaa6cbd1fe1285a7b
inesder <inesderraz@hotmail.fr>
2024-11-19 12:53:32 +0100

    add diagnostic logs

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
 
-const router = require('./routes')
+const app = express();
+console.log('Initializing server...');
 
-const app = express()
-app.use(cors())
+app.use(cors());
+console.log('Middleware CORS initialized.');
+
 const port = process.env.PORT || 3000;
 
-app.use(router)
+app.use(router);
+console.log('Router initialized.');
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.listen(port, () => {
+  console.log(`Magic happens on port ${port}`);
+});
